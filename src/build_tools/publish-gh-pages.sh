c8fbebd634a4468fa82657afbf31c12235d2beee
Hiroshi Ukai <dakusui@gamil.com>
2024-04-23 04:36:10 +0900

    Correct the github-pages site to push content to.

--- src/build_tools/publish-gh-pages.sh
+++ src/build_tools/publish-gh-pages.sh
@@ -9,7 +9,7 @@ _project_name="$(project_name)"
 rm -fr target/site-staging/.git
 cd target/site-staging
 git init
-git remote add origin "https://github.com/dakusui/${_project_name}.git"
+git remote add origin "https://github.com/valid8j/${_project_name}.git"
 git push origin :gh-pages || :
 git checkout -b gh-pages
 git add --all
